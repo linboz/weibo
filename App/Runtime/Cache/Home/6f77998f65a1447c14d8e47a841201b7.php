2d5afe647bb7315626f8fdcb5234ec2e5dd8a69d
Linbo.Zheng <zhenglinbo520@gmail.com>
2016-02-21 16:44:00 +0800

    v0.1
    v0.1

---
+++ App/Runtime/Cache/Home/6f77998f65a1447c14d8e47a841201b7.php
@@ -0,0 +1,17 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>微博系统--登录</title>
+<script type="text/javascript" src="/weibo/PUBLIC/Home/js/jquery.js"></script>
+<script type="text/javascript" src="/weibo/PUBLIC/Home/js/jquery.ui.js"></script>
+<script type="text/javascript" src="/weibo/PUBLIC/Home/js/login.js"></script>
+<link rel="stylesheet" href="/weibo/PUBLIC/Home/css/jquery.ui.css">
+<link rel="stylesheet" href="/weibo/PUBLIC/Home/css/login.css">
+</head>
+<body>
+
+<div id="box">box</div>
+
+</body>
+</html>
